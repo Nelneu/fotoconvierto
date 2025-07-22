c03dbbc62ed79ff592477ee3c0fa5041bfb4e3ce
Nelson Pullella <98887645+Nelneu@users.noreply.github.com>
2025-07-22 10:53:01 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
+// Seleccionamos los elementos por su ID (coinciden con el HTML)
 const fileInput = document.getElementById('fileInput');
 const preview = document.getElementById('preview');
 
+// Evento de cambio en el input
 fileInput.addEventListener('change', () => {
   const file = fileInput.files[0];
+
   if (file && file.type.startsWith('image/')) {
     const reader = new FileReader();
     reader.onload = (e) => {
